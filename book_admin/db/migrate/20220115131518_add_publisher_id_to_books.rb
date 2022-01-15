2db5a6fa3bd88de63599bfe4f9f00baac51b480d
yota-g <yota18493@gmail.com>
2022-01-15 22:16:32 +0900

    create migrate & models

---
+++ book_admin/db/migrate/20220115131518_add_publisher_id_to_books.rb
@@ -0,0 +1,5 @@
+class AddPublisherIdToBooks < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :books, :publisher, null: false, foreign_key: true
+  end
+end
--- book_admin/db/schema.rb
+++ book_admin/db/schema.rb
@@ -10,12 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_152209) do
+ActiveRecord::Schema.define(version: 2022_01_15_131333) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.date "published_on"
     t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
