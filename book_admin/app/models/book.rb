aa6cd8403b24453e0045858c6a01c4ff54492109
yota-g <yota18493@gmail.com>
2022-01-15 23:42:37 +0900

    modified db model

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -2,5 +2,8 @@ class Book < ApplicationRecord
   scope :costly, -> {where("price > ?" , 3000)}
   scope :written_about, ->(theme) {where("name like ?" , "%#{theme}%")}
   scope :find_price , ->(price){ find_by(price: price)}
+  belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
 end
 
